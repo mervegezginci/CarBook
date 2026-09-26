3eaf9af96b822c28afb7e9bcfc78d04e5fc5996b
Merve Gezginci <ymgezginci9@gmail.com>
2026-09-26 15:54:34 +0300

    feature için mediator crud işlemleri eklendi

---
+++ CarBook.Interfaces/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
+    {
+        private readonly IRepository<Feature> _repository;
+        public CreateFeatureCommandHandler(IRepository<Feature> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Feature
+            {
+                Name = request.Name
+            });
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ CarBook.Interfaces/Features/Mediator/Queries/FeatureQueries/GetFeatureByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using CarBook.Application.Features.Mediator.Results.FeatureResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBook.Application.Features.Mediator.Queries.FeatureQueries
+{
+    public class GetFeatureByIdQuery:IRequest<GetFeatureByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetFeatureByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
